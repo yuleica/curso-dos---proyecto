15a5e76d93a2144563ca9516ee097566e84274c1
yuleica <yuleicasojo2005@gmail.com>
2024-10-24 07:35:54 -0300

    primer push proyecto dos

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+      'azul-claro':'#243cff',
+      'azul-oscuro':'#0d1b3e'}
+    },
+  },
+  plugins: [],
+}
+
